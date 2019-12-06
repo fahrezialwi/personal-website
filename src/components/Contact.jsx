e8a3be279aac90beada0c1cf8b68d2ccf5f3d2fa
fahrezialwi <fahrezialwi@gmail.com>
2019-12-06 11:18:11 +0700

    Build contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import Header from './Header'
 import '../styles/contact.css'
 
 class Contact extends Component {
     render() {
         return (
             <div>
-                Contact Component
+                <Header/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h4>Contact Us</h4>
+                            <form>
+                                <input type="text" className="form-control mb-4"  name="name" placeholder="Name" required/>
+                                <input type="email" className="form-control mb-4" name="email" placeholder="E-mail" required/>
+                                <input type="text" className="form-control mb-4" name="subject" placeholder="Subject" required/>
+                                <textarea name="message" className="form-control mb-4" placeholder="Your message" rows="8" required/>
+                                <button className="btn-main">Send</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
